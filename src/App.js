72b09ad0b5ae9bac1c2c51ec5bab36f944c792d0
Isaac G Mwithiga <iceark5@yahoo.com>
2023-01-10 01:09:52 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import './App.css';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-}
+// Reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+// Create a Redux store
+const store = createStore(reducer, { count: 0 });
+
+// Map Redux state to component props
+const mapStateToProps = state => ({
+  count: state.count
+});
+
+// Map Redux actions to component props
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch({ type: 'INCREMENT' }),
+  decrement: () => dispatch({ type: 'DECREMENT' })
+});
+
+// Connected component
+const Counter = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(({ count, increment, decrement }) => (
+  <div>
+    <h2>{count}</h2>
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
+  </div>
+));
+
+// App component
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
 
 export default App;
